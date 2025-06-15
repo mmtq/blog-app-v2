15caba9ff712513e2cb09ee3af4b9c59ddb6cc88
mmtq <mirtarhimul@gmail.com>
2025-06-15 23:51:23 +0600

    get all posts, show in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
-import Image from "next/image";
+import { getAllPosts } from "@/lib/db/queries";
+import { Metadata } from "next";
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: "BlogMMTQ v2",
+  description: "NextJS-15 Blog App using zustand, drizzle, and postgres ",
+}
+
+export default async function Home() {
+
+  const posts = await getAllPosts();
+  console.log(posts);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Home Page
-    </div>
+    <main className="py-10">
+      <div className="max-w-7xl mx-auto px-4">
+        <h1 className="text-2xl font-bold mb-2">Welcome to the Blog</h1>
+        {
+          posts?.length > 0 ? (
+            <div className="mt-4">
+              {posts.map(post => (
+                <div key={post.id} className="mb-4">
+                  <h2 className="text-lg font-semibold">{post.title}</h2>
+                  <p className="text-muted-foreground">{post.content}</p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="text-center py-10">
+              <h2 className="text-xl font-medium">No posts found</h2>
+            </div>
+          )
+        }
+      </div>
+    </main>
   );
 }
---
+++ lib/db/queries.ts
@@ -0,0 +1,20 @@
+'use server'
+
+import { desc } from "drizzle-orm";
+import { db } from ".";
+import { posts } from "./schema";
+
+export async function getAllPosts() {
+    try {
+        const allPosts = await db.query.posts.findMany({
+            orderBy: [desc(posts.createdAt)],
+            with : {
+                author: true
+            }
+        })
+
+        return allPosts
+    } catch (error) {
+        console.error(error);
+    }
+}
